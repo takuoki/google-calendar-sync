968851ef3b04252737c375edbf10a8cb621c7f44
takuoki <takuo.liamg@gmail.com>
2025-03-27 00:08:08 +0900

    interfaceではなく構造体を返すように変更

--- api/domain/service/clock.go
+++ api/domain/service/clock.go
@@ -2,22 +2,17 @@ package service
 
 import "time"
 
-type Clock interface {
-	Now() time.Time
-	Today() time.Time
+type Clock struct{}
+
+func NewClock() *Clock {
+	return &Clock{}
 }
 
-type clock struct{}
-
-func NewClock() Clock {
-	return &clock{}
-}
-
-func (c *clock) Now() time.Time {
+func (c *Clock) Now() time.Time {
 	return time.Now()
 }
 
-func (c *clock) Today() time.Time {
+func (c *Clock) Today() time.Time {
 	loc, _ := time.LoadLocation("Asia/Tokyo")
 	return c.Now().In(loc).Truncate(24 * time.Hour)
 }
